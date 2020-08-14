cf9cc8b24cfdeb8060020b81233eec5978de17d7
Cooper Viktor <im.cooperviktor@gmail.com>
2020-08-14 20:21:24 +1000

    JSX injection via reactDOM

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,13 +3,34 @@
 // console.log("app.js is running");
 
 //* Rendering React to the dom
-var test = React.createElement(
-  "p",
-  null,
-  "tadaa"
+// var test = (
+// 	<div>
+// 		<h1>Title</h1>
+// 		<p>tadaa</p>
+// 	</div>
+// );
+
+var myTemplate = React.createElement(
+	"div",
+	null,
+	React.createElement(
+		"h1",
+		null,
+		"Cooper Viktor"
+	),
+	React.createElement(
+		"p",
+		null,
+		"Age: 35"
+	),
+	React.createElement(
+		"p",
+		null,
+		"Location: Melbourne Australia"
+	)
 );
 var appRoot = document.getElementById("app");
 
 //* ReactDOM uses the extension .render that takes two params
 //* 1st is the JSX being injected; 2nd is the injection point i.e the Div in the HTML
-ReactDOM.render(test, appRoot);
+ReactDOM.render(myTemplate, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 // console.log("app.js is running");
 
 //* Rendering React to the dom
-var test = <p>tadaa</p>;
+// var test = (
+// 	<div>
+// 		<h1>Title</h1>
+// 		<p>tadaa</p>
+// 	</div>
+// );
+
+var myTemplate = (
+	<div>
+		<h1>Cooper Viktor</h1>
+		<p>Age: 35</p>
+		<p>Location: Melbourne Australia</p>
+	</div>
+);
 var appRoot = document.getElementById("app");
 
 //* ReactDOM uses the extension .render that takes two params
 //* 1st is the JSX being injected; 2nd is the injection point i.e the Div in the HTML
-ReactDOM.render(test, appRoot);
+ReactDOM.render(myTemplate, appRoot);
