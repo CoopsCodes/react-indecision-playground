af522d418bce9d47ec7b494988e7e2051c2a7a43
Cooper Viktor <im.cooperviktor@gmail.com>
2020-08-14 21:45:39 +1000

    ternary operators and inline conditional rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,28 +9,95 @@
 // 		<p>tadaa</p>
 // 	</div>
 // );
+//* ------------------------
 
+//* Rendering via variables
+// var userName = "Cooper";
+// var userAge = 35;
+// var userLocation = "Melbourne Australia";
+// var myTemplate = (
+// 	<div>
+// 		<h1>Hi: {userName.toLocaleUpperCase() + "!"}</h1>
+// 		<p>Age: {userAge}</p>
+// 		<p>Location: {userLocation}</p>
+// 	</div>
+// );
+//* ------------------------
+
+//* Rendering via an Object
+var user = {
+	name: "Cooper Viktor",
+	age: 36,
+	location: "Melbourne Victoria"
+};
 var myTemplate = React.createElement(
+	"div",
+	null,
+	"//! Ternary Operator example",
+	React.createElement(
+		"h1",
+		null,
+		"Welcome ",
+		user.name ? user.name : "New User"
+	),
+	"//! And / OR example",
+	user.age && user.age >= 18 && React.createElement(
+		"p",
+		null,
+		"Age: ",
+		user.age
+	),
+	getLocation(user.location)
+);
+//* AND / OR check to see if user age exists and is greater than 18, then it will display result
+//* ------------------------
+
+//* Conditional Rendering
+function getLocation(location) {
+	if (location) {
+		return React.createElement(
+			"p",
+			null,
+			"Location: ",
+			location
+		);
+	}
+	/* If your else is returning undefined, you can remove the whole return
+ else {
+ 	return undefined
+ }
+ */
+}
+
+//* Final app render
+var app = {
+	title: "Indecision App",
+	subtitle: "Let your choices be chosen at random!",
+	option: ["One, Two"]
+};
+
+var template = React.createElement(
 	"div",
 	null,
 	React.createElement(
 		"h1",
 		null,
-		"Cooper Viktor"
+		app.title
+	),
+	app.subtitle && React.createElement(
+		"p",
+		null,
+		app.subtitle
 	),
 	React.createElement(
 		"p",
 		null,
-		"Age: 35"
-	),
-	React.createElement(
-		"p",
-		null,
-		"Location: Melbourne Australia"
+		app.option.length > 0 ? "Here are your options" : "Nothing"
 	)
 );
+
 var appRoot = document.getElementById("app");
 
 //* ReactDOM uses the extension .render that takes two params
 //* 1st is the JSX being injected; 2nd is the injection point i.e the Div in the HTML
-ReactDOM.render(myTemplate, appRoot);
+ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -7,16 +7,68 @@
 // 		<p>tadaa</p>
 // 	</div>
 // );
+//* ------------------------
 
+//* Rendering via variables
+// var userName = "Cooper";
+// var userAge = 35;
+// var userLocation = "Melbourne Australia";
+// var myTemplate = (
+// 	<div>
+// 		<h1>Hi: {userName.toLocaleUpperCase() + "!"}</h1>
+// 		<p>Age: {userAge}</p>
+// 		<p>Location: {userLocation}</p>
+// 	</div>
+// );
+//* ------------------------
+
+//* Rendering via an Object
+var user = {
+	name: "Cooper Viktor",
+	age: 36,
+	location: "Melbourne Victoria",
+};
 var myTemplate = (
 	<div>
-		<h1>Cooper Viktor</h1>
-		<p>Age: 35</p>
-		<p>Location: Melbourne Australia</p>
+		//! Ternary Operator example
+		<h1>Welcome {user.name ? user.name : "New User"}</h1>
+		//! And / OR example
+		{user.age && user.age >= 18 && <p>Age: {user.age}</p>}
+		{getLocation(user.location)}
 	</div>
 );
+//* AND / OR check to see if user age exists and is greater than 18, then it will display result
+//* ------------------------
+
+//* Conditional Rendering
+function getLocation(location) {
+	if (location) {
+		return <p>Location: {location}</p>;
+	}
+	/* If your else is returning undefined, you can remove the whole return
+	else {
+		return undefined
+	}
+	*/
+}
+
+//* Final app render
+var app = {
+	title: "Indecision App",
+	subtitle: "Let your choices be chosen at random!",
+	option: ["One, Two"],
+};
+
+var template = (
+	<div>
+		<h1>{app.title}</h1>
+		{app.subtitle && <p>{app.subtitle}</p>}
+		<p>{app.option.length > 0 ? "Here are your options" : "Nothing"}</p>
+	</div>
+);
+
 var appRoot = document.getElementById("app");
 
 //* ReactDOM uses the extension .render that takes two params
 //* 1st is the JSX being injected; 2nd is the injection point i.e the Div in the HTML
-ReactDOM.render(myTemplate, appRoot);
+ReactDOM.render(template, appRoot);
