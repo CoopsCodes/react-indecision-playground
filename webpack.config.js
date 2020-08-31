ddb3755ecf65f57e8fede95acd6ec7f3117ea685
Cooper Viktor <im.cooperviktor@gmail.com>
2020-08-31 13:59:30 +1000

    integrated webpack with babel and refactored the app back to using it

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,28 @@ const path = require("path");
 // entry -> output
 
 // As output/path needs to be an absolute filepath, console logging the __dirname will provide that.
-// Then by using NodeJS' path extension we can contatinate the two to point to the public directory.
+// Then by using NodeJS' path extension we can concatinate the two to point to the public directory.
 console.log(__dirname);
 
 // module.exports is a NodeJS command that allows the content to be exposed and used in other applications
-// Docs: https://webpack.js.org/concepts/entry-points/
 module.exports = {
+	// Docs: https://webpack.js.org/concepts/entry-points/
 	entry: "./src/app.js",
 	output: {
 		path: path.join(__dirname, "public"),
 		filename: "bundle.js",
+	},
+	// Loader
+	// A loader is a language parser, without the loader this app cant understand the JSX, so we use a loader to 'load' babel and translate
+	// other examples would be an SCSS compiler that changes your SCSS files down to CSS
+	// https://webpack.js.org/configuration/module/#modulerules
+	module: {
+		rules: [
+			{
+				loader: "babel-loader",
+				test: /\.js$/,
+				exclude: /node_modules/,
+			},
+		],
 	},
 };
