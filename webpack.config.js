c5e2cb065b385fd33d41af218cdec6e8229f0a9f
Cooper Viktor <im.cooperviktor@gmail.com>
2020-09-07 13:10:10 +1000

    reverting back to a stable build after breaking: SCSS working in this build

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,17 @@ module.exports = {
 				test: /\.js$/,
 				exclude: /node_modules/,
 			},
+			{
+				test: /\.scss$/,
+				use: [
+					// Creates `style` nodes from JS strings
+					"style-loader",
+					// Translates CSS into CommonJS
+					"css-loader",
+					// Compiles Sass to CSS
+					"sass-loader",
+				],
+			},
 		],
 	},
 	// https://webpack.js.org/configuration/devtool/
